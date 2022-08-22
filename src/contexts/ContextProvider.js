d953920c304fd3699479de0bebbd9cf18a696ba3
Pietro Oliveira <pietromartinso@gmail.com>
2022-08-22 09:33:12 -0300

    Starting e-commerce page

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,42 @@
+import React, { createContext, useContext, useState } from 'react'
+
+const StateContext = createContext();
+
+const initialState = {
+    chat: false,
+    cart: false,
+    userProfile: false,
+    notification: false,
+}
+
+export const ContextProvider = ({ children }) => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined);
+
+    const handleClick = (clicked) => {
+        setIsClicked({...initialState, [clicked]: true });
+    }
+
+    console.log("[DEBUG] ContextProvider!")
+    return (
+        <StateContext.Provider 
+            value={{
+                activeMenu, 
+                setActiveMenu,
+                isClicked, 
+                setIsClicked,
+                handleClick,
+                screenSize, 
+                setScreenSize
+            }}
+        >
+            {children}
+        </StateContext.Provider>
+    ) 
+};
+
+export const useStateContext = () => {
+    console.log("[DEBUG] UseStateContext:", StateContext)
+    return useContext(StateContext)
+};
